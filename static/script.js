1a19a3433a7ad0ea9d5f9be69439db2bad77a265
minieetea <kang@mikyung.me>
2020-09-07 21:06:47 +0900

    #4 탭을 분리한다
    깜박거리는 이슈는 해결하지 못했다..

--- static/script.js
+++ static/script.js
@@ -93,3 +93,83 @@ function appendHoricard(title, url, desc, author, image, price, isbn) {
         </div>`
     $('#book-info').append(horicard);
 }
+
+function myWishlist() {
+    console.log('위시리스트 조회 시작');
+    <!--위시리스트에 전부 조회하기-->
+    $.ajax({
+        type: "GET",
+        url: "/getWishlist",
+        data: {},
+        success: function (response) {
+            console.log(response["msg"])
+            if (response["result"] == "success") {
+                console.log("왓다");
+                let wishlist = response["wishbooks"]
+                for (let i = 0; i < wishlist.length; i++) {
+                    let title = wishlist[i]['title'];
+                    let desc = wishlist[i]['desc'];
+                    let image = wishlist[i]['image'];
+                    let url = wishlist[i]['url'];
+                    let author = wishlist[i]['author'];
+                    let price = wishlist[i]['price'];
+                    let isbn = wishlist[i]['isbn'];
+                    appendHoricard2(title, url, desc, author, image, price, isbn);
+                }
+            }
+        }
+    });
+}
+
+function removeWishlist() {
+    let isbn = $('#isbn').text();
+    $.ajax({
+        type: "POST",
+        url: "/removeWishlist",
+        data: {isbn: isbn},
+        success: function (response) { // 성공하면
+            if (response["result"] == "success")
+                console.log("지웠다");
+            window.location.reload();
+        }
+    })
+}
+
+function appendHoricard2(title, url, desc, author, image, price, isbn) {
+    let horicard = `<div class="card">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="${image}" class="card-img" id="bookcover">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <a id="store-url" href="${url}"><h5 class="card-title" id="title">${title}</h5></a>
+                        <p id="isbn">${isbn}</p>
+                        <p class="card-text"><small class="text-muted" id="author">${author}</small><small class="text-muted" id="price">${price}</small></p>
+                        <p class="card-text" id="desc">${desc}</p>
+                        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                          <div class="btn-group mr-2" role="group" aria-label="button group">
+                            <button type="button" class="btn btn-primary"><i class="far fa-credit-card"></i> 복</button>
+                            <button type="button" class="btn btn-secondary"><i class="fas fa-wallet"></i> 불복</button>
+                          </div>
+                          <div class="btn-group mr-1" role="group" aria-label="third group">
+                            <button type="button" class="btn btn-light"><i class="fas fa-retweet"></i> 빌려보기</button>
+                          </div>
+                          <div class="btn-group mr-1" role="group" aria-label="second group">
+                            <button type="button" class="btn btn-danger" onclick="removeWishlist()"><i class="fas fa-minus-circle"></i> 마음이 바뀜</button>
+                          </div>
+                          <div class="alert alert-danger" role="alert">
+                              <i class="fas fa-exclamation-triangle"> </i><a href="#" class="alert-link">보유 중인 책(복)</a>입니다. 사기전에 다시 생각하세요.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    $('#book-info').append(horicard);
+}
+
+function bottomtab() {
+    $(this).addClass('active');
+    $(this).parent().children('a').not(this).removeClass('active');
+}
